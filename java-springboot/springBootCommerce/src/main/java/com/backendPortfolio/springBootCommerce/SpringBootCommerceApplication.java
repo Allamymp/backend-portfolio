098fed4eed89c52b83d33a69f073f84a6c4fe6f9
allamymp <allamympereira@gmail.com>
2023-05-24 14:20:44 -0300

    project created

---
+++ java-springboot/springBootCommerce/src/main/java/com/backendPortfolio/springBootCommerce/SpringBootCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.backendPortfolio.springBootCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommerceApplication.class, args);
+	}
+
+}

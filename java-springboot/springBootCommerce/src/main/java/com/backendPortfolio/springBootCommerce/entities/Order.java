1c88972513fbbffe3c1d6fb788976d138d19fc2b
allamymp <allamympereira@gmail.com>
2023-05-30 15:11:44 -0300

    Payment one to one association

--- java-springboot/springBootCommerce/src/main/java/com/backendPortfolio/springBootCommerce/entities/Order.java
+++ java-springboot/springBootCommerce/src/main/java/com/backendPortfolio/springBootCommerce/entities/Order.java
@@ -29,6 +29,8 @@ public class Order implements Serializable {
     private Integer orderStatus;
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -74,7 +76,16 @@ public class Order implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
- public Set<OrderItem> getItems(){
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems(){
         return items;
  }
     @Override

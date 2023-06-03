0b0844b53607830e33180d6e34a6d48e1935cee1
allamymp <allamympereira@gmail.com>
2023-06-03 20:58:38 -0300

    exception handler update()

--- java-springboot/springBootCommerce/src/main/java/com/backendPortfolio/springBootCommerce/services/UserService.java
+++ java-springboot/springBootCommerce/src/main/java/com/backendPortfolio/springBootCommerce/services/UserService.java
@@ -48,7 +48,7 @@ public class UserService {
     }
 
 
-    public User update(Long id, User obj) {
+    public User  update(Long id, User obj) {
         try {
             User entity = repository.getReferenceById(id);
             updateData(entity, obj);

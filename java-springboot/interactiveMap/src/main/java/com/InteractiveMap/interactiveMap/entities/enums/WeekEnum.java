007dda682d058a60811b776640ffc2bd1cff7f55
allamymp <allamympereira@gmail.com>
2023-06-21 14:55:53 -0300

    enum updated

---
+++ java-springboot/interactiveMap/src/main/java/com/InteractiveMap/interactiveMap/entities/enums/WeekEnum.java
@@ -0,0 +1,12 @@
+package com.InteractiveMap.interactiveMap.entities.enums;
+
+public enum WeekEnum {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+
+}

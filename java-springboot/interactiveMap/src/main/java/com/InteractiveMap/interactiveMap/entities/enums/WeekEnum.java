bdb5411a48272a84061a2b4e9e173704134684fb
allamymp <allamympereira@gmail.com>
2023-06-26 15:06:55 -0300

    enum code added

--- java-springboot/interactiveMap/src/main/java/com/InteractiveMap/interactiveMap/entities/enums/WeekEnum.java
+++ java-springboot/interactiveMap/src/main/java/com/InteractiveMap/interactiveMap/entities/enums/WeekEnum.java
@@ -1,12 +1,18 @@
 package com.InteractiveMap.interactiveMap.entities.enums;
 
 public enum WeekEnum {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
+    SUNDAY(1),
+    MONDAY(2),
+    TUESDAY(3),
+    WEDNESDAY(4),
+    THURSDAY(5),
+    FRIDAY(6),
+    SATURDAY(7),
+    ;
 
+   private int code;
+
+   private WeekEnum(int code){
+       this.code = code;
+   }
 }
